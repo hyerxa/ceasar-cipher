2b40c0f46b2f565d71fce766a29a9ce3bdd83f41
hyerxa <haley.yerxa24@gmail.com>
2019-04-23 19:29:18 -0400

    First real commit

---
+++ js/ceasar_cipher.js
@@ -0,0 +1,43 @@
+function shift() {
+    var word = document.getElementById("input").value;
+    var newWord = new String();
+    var letter;
+    var alphabet = /[a-z]/i;
+    var xyz = /[x-z]/i;
+    for (let i = 0; i < word.length; i++) {
+        if (alphabet.test(word[i]) === false) {
+            letter = word[i];
+        }
+        else if (word[i].toLowerCase() === 'x') {
+            if (word[i] != 'x') {
+                letter = 'A';
+            }
+            else {
+                letter = 'a';
+            }
+        }
+        else if (word[i].toLowerCase() === 'y') {
+            if (word[i] != 'y') {
+                letter = 'B';
+            }
+            else {
+                letter = 'b';
+            }
+        }
+        else if (word[i].toLowerCase() === 'z') {
+            if (word[i] != 'z') {
+                letter = 'C';
+            }
+            else {
+                letter = 'c';
+            }
+        }
+        else {
+            var oldCode = word.charCodeAt(i);
+            var newCode = (oldCode + 3);
+            letter = String.fromCharCode(newCode);
+        }
+        newWord = newWord + letter;
+    }
+    document.getElementById("message").innerHTML = ("Your encrypted message is: " + newWord);
+}
